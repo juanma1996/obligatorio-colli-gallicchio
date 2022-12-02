a18c3e6adc1e79b7020cf17f1b4a302495cf35bf
Juan Gallicchio <jgallicchio@sylvanroad.com>
2022-12-02 19:38:53 -0300

    Adding addAddressToBlackList to BridgeContractAbstract

--- contracts/BridgeContractAbstract.sol
+++ contracts/BridgeContractAbstract.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.16;
 
 abstract contract BridgeContractAbstract {
 
+    /// STATE VARIABLES
     address private _owner;
+
+    /// STATE MAPPINGS
+    mapping(address => bool) public _blacklistAddress;
 
     constructor()
     {
@@ -13,5 +17,31 @@ abstract contract BridgeContractAbstract {
 
     function owner() public virtual view returns (address){
         return _owner;
+    }
+
+    function addAddressToBlackList(address _invalidAddress) public virtual{
+        _isZeroAddress(_invalidAddress, '_invalidAddress');
+        if (_owner == _invalidAddress) {
+            revert("Invalid address _invalidAddress");
+        }
+        _isBlacklistAddress(_invalidAddress);
+        _blacklistAddress[_invalidAddress] = true;
+    }
+
+    /// ------------------------------------------------------------------------------------------------------------------------------------------
+    /// PRIVATE FUNCTIONS
+    /// ------------------------------------------------------------------------------------------------------------------------------------------
+
+    function _isZeroAddress(address _address, string memory _parameterName) internal virtual pure {
+        if (_address == address(0)) {
+            string memory _message = string.concat( _parameterName, " cannot be zero address");
+            revert(_message);
+        }
+    }
+
+    function _isBlacklistAddress(address _address) internal virtual {
+        if (_blacklistAddress[_address]) {
+            revert("Address already in the list");
+        }
     }
 }
